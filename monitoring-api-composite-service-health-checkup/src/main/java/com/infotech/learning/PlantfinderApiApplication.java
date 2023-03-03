6427d95c718f5eef7626e4e7edb4a3f741b2d7bd
InfoTech <34073095+ganeshmali50@users.noreply.github.com>
2023-03-03 16:14:53 +0530

    monitoring-api-composite-service-health-checkup
    monitoring-api-composite-service-health-checkup

---
+++ monitoring-api-composite-service-health-checkup/src/main/java/com/infotech/learning/PlantfinderApiApplication.java
@@ -0,0 +1,13 @@
+package com.infotech.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantfinderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantfinderApiApplication.class, args);
+	}
+
+}
